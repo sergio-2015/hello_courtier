44c816aba000edd18530f0378c3c547b491ec233
sergio monteiro <sergio.perso@gmail.com>
2016-06-11 00:50:56 +0200

    ok pas mal de changements

--- app/models/broker.rb
+++ app/models/broker.rb
@@ -5,8 +5,11 @@ class Broker < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :broker_agencies
 
-  validates :first_name, :last_name, :email, presence: true, unless: :new_record?
+  validates :first_name, :last_name, :email, presence: true
   validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
   # validates_format_of :phone_number, :with => /\A0[0-9]([-. ]?\d{2}){4}[-. ]?\z/
 
 end
+
+
+# , unless: :new_record?
